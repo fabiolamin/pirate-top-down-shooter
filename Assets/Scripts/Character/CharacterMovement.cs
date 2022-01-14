fab9aebc8174afc32c3b213731ca2044875a3938
fabiolamin <fabio_lamin@hotmail.com>
2022-01-14 15:02:37 -0300

    Create the Character Movement

---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace PirateGame.Character
+{
+    public class CharacterMovement : MonoBehaviour
+    {
+        [SerializeField] private float _movementSpeed = 10f;
+        [SerializeField] private float _rotationSpeed = 5f;
+
+        public void Move(Vector2 newPosition)
+        {
+            float speed = _movementSpeed * Time.deltaTime;
+            transform.position += new Vector3(newPosition.x * speed, newPosition.y * speed, transform.position.z);
+
+            Rotate(newPosition);
+        }
+
+        private void Rotate(Vector2 newPosition)
+        {
+            if (newPosition.magnitude != 0f)
+            {
+                float zDegree = Mathf.Atan2(newPosition.y, newPosition.x) * Mathf.Rad2Deg;
+                Quaternion newRotation = Quaternion.Euler(0f, 0f, zDegree);
+                transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, _rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
+

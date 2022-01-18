326341b20426a2a177cc555c0ff3e36cc332179e
fabiolamin <fabio_lamin@hotmail.com>
2022-01-18 13:17:50 -0300

    Abstract AI Agent

--- Assets/Scripts/AI/Enemy/EnemyAI.cs
+++ Assets/Scripts/AI/Enemy/EnemyAI.cs
@@ -1,66 +1,15 @@
 using PirateGame.AI.Path;
-using PirateGame.Movement;
-using UnityEngine;
 
 namespace PirateGame.AI.Enemy
 {
     public abstract class EnemyAI : AIAgent
     {
-        private bool _isReadyToMove = false;
-
-        [SerializeField] private PathFinder _pathFinder;
-        [SerializeField] private CharacterMovementEvent _onEnemyMoved;
-        [SerializeField] private Collider2D _collider;
-
-        public PathFinder PathFinder { get { return _pathFinder; } }
-        public Collider2D Collider { get { return _collider; } }
-
         public override void StartAIMovement()
         {
-            Waypoint startingWaypoint = PathFinder.WaypointData.GetStartingWaypoint(_collider);
+            Waypoint startingWaypoint = PathFinder.WaypointData.GetStartingWaypoint(Collider);
             PathFinder.GetReadyToFindNewPath(startingWaypoint, GetTarget());
-            _isReadyToMove = true;
+            isReadyToMove = true;
         }
-
-        private void Update()
-        {
-            Move();
-        }
-
-        private void Move()
-        {
-            if (_isReadyToMove)
-            {
-                if (!PathFinder.IsReadyToFindNewPath)
-                {
-                    _onEnemyMoved.Invoke(GetNewPosition());
-                    CheckNewPath();
-                }
-            }
-        }
-
-        private Vector2 GetNewPosition()
-        {
-            if (_pathFinder.HasReachedNextWaypoint(transform.position))
-            {
-                _pathFinder.UpdateNextWaypoint();
-            }
-
-            Vector2 newPosition = (_pathFinder.NextWaypoint.transform.position - transform.position).normalized;
-
-            return newPosition;
-        }
-
-        private void CheckNewPath()
-        {
-            if (PathFinder.IsPathComplete(Collider))
-            {
-                Waypoint startingWaypoint = PathFinder.WaypointData.GetStartingWaypoint(Collider);
-                PathFinder.GetReadyToFindNewPath(startingWaypoint, GetTarget());
-            }
-        }
-
-        protected abstract Transform GetTarget();
     }
 }
 
--- Assets/Scripts/AI/Path/AIAgent.cs
+++ Assets/Scripts/AI/Path/AIAgent.cs
@@ -1,9 +1,58 @@
+using PirateGame.Movement;
 using UnityEngine;
 
 namespace PirateGame.AI.Path
 {
     public abstract class AIAgent : MonoBehaviour
     {
+        [SerializeField] private PathFinder _pathFinder;
+        [SerializeField] private CharacterMovementEvent _onAgentMoved;
+        [SerializeField] private Collider2D _collider;
+
+        protected bool isReadyToMove = false;
+
+        public PathFinder PathFinder { get { return _pathFinder; } }
+        public Collider2D Collider { get { return _collider; } }
+
+        private void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            if (isReadyToMove)
+            {
+                if (!PathFinder.IsReadyToFindNewPath)
+                {
+                    _onAgentMoved.Invoke(GetNewPosition());
+                    CheckNewPath();
+                }
+            }
+        }
+
+        private Vector2 GetNewPosition()
+        {
+            if (_pathFinder.HasReachedNextWaypoint(transform.position))
+            {
+                _pathFinder.UpdateNextWaypoint();
+            }
+
+            Vector2 newPosition = (_pathFinder.NextWaypoint.transform.position - transform.position).normalized;
+
+            return newPosition;
+        }
+
+        private void CheckNewPath()
+        {
+            if (PathFinder.IsPathComplete(Collider))
+            {
+                Waypoint startingWaypoint = PathFinder.WaypointData.GetStartingWaypoint(Collider);
+                PathFinder.GetReadyToFindNewPath(startingWaypoint, GetTarget());
+            }
+        }
+
+        protected abstract Transform GetTarget();
         public abstract void StartAIMovement();
     }
 }

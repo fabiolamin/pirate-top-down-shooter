dc01b4be482294eb2e8df27438f20cd140381788
fabiolamin <fabio_lamin@hotmail.com>
2022-01-20 10:53:18 -0300

    Fix ship's health bar

---
+++ Assets/Scripts/Utils/FollowBar.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace PirateGame.Utils
+{
+    public class FollowBar : MonoBehaviour
+    {
+        private Transform _target;
+        private Vector3 _offset;
+
+        private void Awake()
+        {
+            _target = transform.root;
+            _offset = transform.position - _target.position;
+        }
+
+        private void LateUpdate()
+        {
+            transform.position = _target.position + _offset;
+            transform.LookAt(Camera.main.transform.position);
+        }
+    }
+}
+
